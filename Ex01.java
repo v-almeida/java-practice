adc801c338514643e3cdc368fed3a052dc1d7426
v-almeida <almeida.vitor20000@gmail.com>
2026-01-05 20:02:38 -0300

    Ex01 done

--- Ex01.java
+++ Ex01.java
@@ -1,3 +1,15 @@
+// 1. Escreva um algoritmo que calcule a área de um triângulo cuja fórmula é base x altura / 2.
+
 public class Ex01 {
     
+    public static void executar() {
+
+        int base = 5;
+        int altura = 2;
+        int resultado;
+        resultado = base * altura / 2;
+
+        System.out.println("A área do triângulo é " + resultado + "!");
+
+    }
 }
