280e3a236f7eb76b1679792c398d90e458f275aa
v-almeida <almeida.vitor20000@gmail.com>
2026-01-12 19:18:28 -0300

    01 list down!

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     
     public static void main(String[] args){
-        
-        //Ex11.executar();
 
-        System.out.println("11 down, 4 to go...");
+        System.out.println("The first list is done!");
+        
+        //Ex16.executar();
         
     }
 }
