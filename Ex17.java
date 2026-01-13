981238abb91cabcd1b0049db7cd72327b6c2e6d6
v-almeida <almeida.vitor20000@gmail.com>
2026-01-13 20:14:36 -0300

    Ex17 done

---
+++ Ex17.java
@@ -0,0 +1,11 @@
+// 17. Escreva um algoritmo que mostre os números de 1 até 10, um número por linha.
+
+public class Ex17 {
+    
+    public static void executar(){
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    } 
+}
