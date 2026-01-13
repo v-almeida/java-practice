7bdaa1c83c2f75fcf381736f9e4e827ee5f909ba
v-almeida <almeida.vitor20000@gmail.com>
2026-01-13 20:54:37 -0300

    Ex21 done

---
+++ Ex21.java
@@ -0,0 +1,24 @@
+/*5. Escreva um algoritmo que mostre todos os números pares existentes entre 1 e um número lido do
+teclado, bem como a quantidade de números apresentados. */
+
+import java.util.Scanner;
+public class Ex21 {
+
+    public static void executar(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = scanner.nextInt();
+
+        int i;
+
+        for (i = 1; i <= num; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+            else {
+                System.out.println("Não existem números pares existentes!");
+            }
+        }
+    }
+}
