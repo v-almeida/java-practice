7ef83e18864d1e7b69993becb0fa548e7382ad70
v-almeida <almeida.vitor20000@gmail.com>
2026-01-14 19:17:26 -0300

    Ex22 done

---
+++ Ex22.java
@@ -0,0 +1,26 @@
+/*6. Escreva um algoritmo que some os números fornecidos pelo usuário até que o número lido seja
+igual a zero. Ao final, mostre o resultado da soma. */
+
+import java.util.Scanner;
+
+public class Ex22 {
+    
+    public static void executar(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int soma = 0;
+        int num;
+
+        do {
+
+        System.out.print("Digite um número: ");
+        num = scanner.nextInt();
+        soma += num;
+
+        }while (num != 0);
+
+        System.out.println("Resultado: " + soma);
+
+    }
+}
