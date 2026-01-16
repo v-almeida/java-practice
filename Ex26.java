7218df4c8549c70a7f54ae9f08d3bcd242f69939
v-almeida <almeida.vitor20000@gmail.com>
2026-01-16 16:33:48 -0300

    Just kidding, Ex26 vectors and arrays Baby... LFGGG

---
+++ Ex26.java
@@ -0,0 +1,19 @@
+/*1. Escreva um programa que alimente um vetor com 5 números inteiros e apresente na tela o vetor em
+ordem inversa. */
+
+public class Ex26 {
+    
+    public static void executar(){
+
+        int[] v = {1,2,3,4,5};
+        int i;
+
+        for (i = 0; i <= 4; i++){
+            System.out.printf("%d\n", v[i]);
+        }
+
+        for (i = 4; i >= 0; i--){
+            System.out.printf("%d\n", v[i]);
+        }
+    }
+}
