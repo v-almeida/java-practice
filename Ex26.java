16e089e2c6db9cea1144431d53a29d12cd8e4e38
v-almeida <almeida.vitor20000@gmail.com>
2026-01-16 18:13:05 -0300

    Ex26 done

--- Ex26.java
+++ Ex26.java
@@ -8,12 +8,9 @@ public class Ex26 {
         int[] v = {1,2,3,4,5};
         int i;
 
-        for (i = 0; i <= 4; i++){
-            System.out.printf("%d\n", v[i]);
-        }
-
         for (i = 4; i >= 0; i--){
             System.out.printf("%d\n", v[i]);
         }
+
     }
 }
